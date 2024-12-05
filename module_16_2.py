1828afe0177a0c3f1c2006f24c760e96cebd98f5
valery-kiselev <83948789+valery-kiselev@users.noreply.github.com>
2024-12-05 17:12:29 +0400

    Update module_16_2.py

--- module_16_2.py
+++ module_16_2.py
@@ -6,15 +6,15 @@ from typing import Annotated
 app = FastAPI()
 
 @app.get('/')
-async def main_page() -> dict:
+async def get_main() -> dict:
     return {'message': 'Главная страница'}
 
 @app.get('/user/admin')
-async def admin_page() -> dict:
+async def get_admin() -> dict:
     return {'message': 'Вы вошли как администратор'}
 
 @app.get('/user/{username}/{age}')
-async def user_name(
+async def read_name(
         username: Annotated[str, Path(min_length=5, max_length=20,
                                       example='UrbanUser', description='Enter username')],
         age: Annotated[int, Path(ge=18, le=120, example=24, description=(
@@ -22,6 +22,6 @@ async def user_name(
     return {'message': f'Информация о пользователе. Имя: {username}, Возраст: {age}'}
 
 @app.get('/user/{user_id}')
-async def user_id(user_id: Annotated[int, Path(ge=1, le=100, example=1,
+async def read_id(user_id: Annotated[int, Path(ge=1, le=100, example=1,
                                                description='The ID must be a positive integer')]) -> dict:
     return {'message': f'Вы вошли как пользователь № {user_id}'}
