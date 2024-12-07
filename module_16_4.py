ff1e47b15fbbcc4d7eeb99b9521730a9a7d0cf00
valery-kiselev <83948789+valery-kiselev@users.noreply.github.com>
2024-12-07 12:26:43 +0400

    Update module_16_4.py

--- module_16_4.py
+++ module_16_4.py
@@ -18,9 +18,9 @@ async def get_user() -> List[User]:
     return users
 
 @app.post('/user/{username}/{age}', response_model=User)
-async def create_user(user: User) -> str:
+async def create_user(username: str, age: int) -> str:
     new_id = max((u.id for u in users), default=0) + 1
-    new_user = User(id=new_id, username=user.username, age=user.age)
+    new_user = User(id=new_id, username=username, age=age)
     users.append(new_user)
     return new_user
 
@@ -33,10 +33,10 @@ async def update_user(user_id: int, user: User):
             return u
     raise HTTPException(status_code=404, detail="User was not found")
 
-@app.delete('/user/{user_id}', response_model=str)
+@app.delete('/user/{user_id}', response_model=User)
 async def delete_user(user_id: int):
     for u in users:
         if u.id == user_id:
             users.remove(u)
-            return f"Пользователь с ID {user_id} удален."
+            return u
     raise HTTPException(status_code=404, detail="User was not found")
